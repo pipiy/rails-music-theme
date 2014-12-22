71910490c7fa4b94d15dc08c5e38779cab569ac2
Artem Slabodnik <artem.slabodnik@gmail.com>
2014-12-23 02:24:03 +0300

    trying

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var express = require('express');
 var app = express();
 
 app.use(gzippo.staticGzip("" + __dirname + "/public"));
-app.listen(process.env.PORT || 3003);
+app.listen(process.env.PORT || 3000);
 
